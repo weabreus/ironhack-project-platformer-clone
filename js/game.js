a58c073c64d4b06625e3e368f6387fd7529590be
Wellington Abreu <wellington.abreu@gmail.com>
2023-11-21 11:01:22 -0400

    Implemented canvas and player basic functionality

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ class Game {
     this.startScreen = document.getElementById("game-intro");
     this.gameScreen = document.getElementById("game-container");
     this.gameEndScreen = document.getElementById("game-end");
+    this.canvas = document.querySelector('canvas');
+    this.canvasContext = this.canvas.getContext('2d');
     this.player = null;
     this.obstacles = [];
     this.score = 0;
@@ -11,18 +13,39 @@ class Game {
   }
 
   start() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    
     this.startScreen.style.display = "none";
     this.gameScreen.style.display = "block";
+    this.player = new Player(this.canvas, this.canvasContext, {x: 100, y:100});
+    this.player.draw()
     this.gameLoop();
   }
 
   gameLoop() {
     if (this.isGameOver) return;
     this.update();
+
+    this.player.velocity.x = 0;
+    if (this.player.keys.d.pressed) {
+        this.player.velocity.x = 5;
+    } else if (this.player.keys.a.pressed) {
+        this.player.velocity.x = -5;
+    } 
+
     window.requestAnimationFrame(() => this.gameLoop());
   }
 
   update() {
+    // Clear the canvas in each frame
+    this.canvasContext.fillStyle = 'white';
+    this.canvasContext.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // Update the player position in each frame
+    this.player.move();
+
     if(this.lives === 0) {
         this.endGame();
     }
