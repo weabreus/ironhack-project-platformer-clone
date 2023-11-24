dd98d2e2096df545dff7a20f8af8066129000a4a
Wellington Abreu <wellington.abreu@gmail.com>
2023-11-24 01:21:57 -0400

    Implmented basic sprites

--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,15 @@ const collision = ({
         object.position.x + object.width >= collisionBlock.position.x
         );
 }
+
+const platformCollision = ({
+    object,
+    collisionBlock
+}) => {
+    return (
+        object.position.y + object.height >= collisionBlock.position.y &&
+        object.position.y + object.height <= collisionBlock.position.y + collisionBlock.height &&
+        object.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        object.position.x + object.width >= collisionBlock.position.x
+        );
+}
